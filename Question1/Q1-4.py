d4e0b3c9ee3ec4f671b9b0adefe2052c011d97be
Eunice <1738101228@qq.com>
2025-04-28 13:35:54 +0800

    update code for question 1-4

--- Question1/Q1-4.py
+++ Question1/Q1-4.py
@@ -20,3 +20,35 @@ if __name__ == "__main__":
     """
     注意：请在下方开始答题，请勿修改上方的代码!!!
     """
+    # 1. 构建CNOT门和H门实现的SWAP电路
+    circuit_cnot_h_swap = QCircuit()
+    circuit_cnot_h_swap << CNOT(qbits[0], qbits[1])
+    circuit_cnot_h_swap << H(qbits[0])
+    circuit_cnot_h_swap << H(qbits[1])
+    circuit_cnot_h_swap << CNOT(qbits[0], qbits[1])
+    circuit_cnot_h_swap << H(qbits[0])
+    circuit_cnot_h_swap << H(qbits[1])
+    circuit_cnot_h_swap << CNOT(qbits[0], qbits[1])
+
+    # 2. 构建直接使用SWAP门的电路
+    circuit_swap = QCircuit()
+    circuit_swap << SWAP(qbits[0], qbits[1])
+
+    # 3. 获取两个电路的矩阵表示
+    from pyqpanda import *
+    matrix_cnot_swap = np.matrix(get_matrix(circuit_cnot_h_swap, False)).reshape((4, 4))
+    matrix_swap = np.matrix(get_matrix(circuit_swap, False)).reshape((4, 4))
+
+    # 4. 比较两个矩阵
+    are_equal = compare_matrices(matrix_cnot_swap, matrix_swap)
+
+    # 5. 打印比较结果
+    print("三个CNOT门构造的SWAP门的矩阵:")
+    print(matrix_cnot_swap)
+    print("\n普通SWAP门的矩阵:")
+    print(matrix_swap)
+
+    if are_equal:
+        print("\n得证，三个CNOT构造的电路可实现SWAP功能")
+
+    qvm.finalize()
